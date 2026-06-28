12e1fc57d3da24cb1914448239f53544febcef12
Crogprince <richoirsyad07@gmail.com>
2026-06-28 22:50:15 +0700

    🔐 Add Netlify Identity widget

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import netlifyIdentity from 'netlify-identity-widget'
+netlifyIdentity.on('init', user => {
+  if (!user) {
+    netlifyIdentity.on('login', () => {
+      document.location.href = '/admin/'
+    })
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
